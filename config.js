232bcb7898c302a2152ea3e9c36ad2543848594b
Ivan Guillermo Lopez <ivanglopezp@gmail.com>
2026-08-19 15:44:24 -0400

    Add base configuration for Pstore

--- config.js
+++ config.js
@@ -1,1 +1,25 @@
+// ==========================================
+// CONFIGURACIÓN BASE LOCAL Y DE RESPALDO
+// ==========================================
+
+const CONFIG_PSTORE = {
+  // 1. Identidad y Contacto de la Tienda
+  nombreTienda: "Pstore",
+  urlLogo: "assets/pstore.jpg",
+  
+  
+  // Redes Sociales (Opcionales)
+  linkInstagram: "https://www.instagram.com/pstorevzla",
+  linkFacebook: "https://www.facebook.com/people/Pstore-Variedades/100069365366035/",
+
+  // 2. Moneda Alternativa
+  // Importante: No colocamos tasa por defecto (null) para no mostrar valores desactualizados.
+  
+
+  // 3. Enlace de publicación CSV de tu pestaña 'Configuracion' en Google Sheets
+  urlSheetConfig: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_D4Cym7p0ATsh5UCG2Q3kbvhy5WuMPx0Q8gCfdz_l9IDoaCb4jn1T8zQ9YKCCvt-0GA0vkDrwKXX2/pub?gid=1458625703&single=true&output=csv",
+
+  // 4. Estilos CSS por defecto (Variables :root)
+  
+};
 
