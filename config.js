e5d69ab3f85a0a8d5e1183a62c8ba12782e74efb
Ivan Guillermo Lopez <ivanglopezp@gmail.com>
2026-08-19 15:49:07 -0400

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,9 @@ const CONFIG_PSTORE = {
   // 1. Identidad y Contacto de la Tienda
   nombreTienda: "Pstore",
   urlLogo: "assets/pstore.jpg",
+  numeroWhatsapp: "+584126216661",
+  mensajeBienvenida: "¡Bienvenidos a Pstore!",
+  urlPdfCatalogo: "https://drive.google.com/file/d/1UbvpO9gN32uocysk-97mQ3RFlLUp-R9q/view?usp=sharing",
   
   
   // Redes Sociales (Opcionales)
@@ -14,7 +17,7 @@ const CONFIG_PSTORE = {
 
   // 2. Moneda Alternativa
   // Importante: No colocamos tasa por defecto (null) para no mostrar valores desactualizados.
-  
+  simboloMonedaAlt: "Bs.",
 
   // 3. Enlace de publicación CSV de tu pestaña 'Configuracion' en Google Sheets
   urlSheetConfig: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_D4Cym7p0ATsh5UCG2Q3kbvhy5WuMPx0Q8gCfdz_l9IDoaCb4jn1T8zQ9YKCCvt-0GA0vkDrwKXX2/pub?gid=1458625703&single=true&output=csv",
@@ -29,6 +32,4 @@ const CONFIG_PSTORE = {
     "--text-secondary": "#a0a0a0",
     "--border-color": "#333333"
   }
-  
 };
-
