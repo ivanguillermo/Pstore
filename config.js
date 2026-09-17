ed757de7f4234f21759dca1eca0eafd292ffb5cd
Ivan Guillermo Lopez <ivanglopezp@gmail.com>
2026-09-16 22:34:40 -0400

    Update config.js

--- config.js
+++ config.js
@@ -8,20 +8,17 @@ const CONFIG_PSTORE = {
   urlLogo: "assets/pstore.jpg",
   numeroWhatsapp: "+584126216661",
   mensajeBienvenida: "¡Bienvenidos a Pstore!",
-  urlPdfCatalogo: "https://drive.google.com/file/d/1UbvpO9gN32uocysk-97mQ3RFlLUp-R9q/view?usp=sharing",
-  
+  urlPdfCatalogo: "https://drive.google.com/file/d/1UbvpO9gN32uocysk-97mQ3RFlLUp-R9q/view?usp=sharing",  
   
   // Redes Sociales (Opcionales)
   linkInstagram: "https://www.instagram.com/pstorevzla",
   linkFacebook: "https://www.facebook.com/people/Pstore-Variedades/100069365366035/",
 
-  // 2. Moneda Alternativa
-  // Importante: No colocamos tasa por defecto (null) para no mostrar valores desactualizados.
+  // 2. Moneda Alternativa  
   simboloMonedaAlt: "Bs.",
 
   // 3. Enlace de publicación CSV de tu pestaña 'Configuracion' en Google Sheets
   urlSheetConfig: "https://docs.google.com/spreadsheets/d/e/2PACX-1vQ_D4Cym7p0ATsh5UCG2Q3kbvhy5WuMPx0Q8gCfdz_l9IDoaCb4jn1T8zQ9YKCCvt-0GA0vkDrwKXX2/pub?gid=1458625703&single=true&output=csv",
-
-  // 4. Estilos CSS por defecto (Variables :root)
+  scriptUrl:"https://script.google.com/macros/s/AKfycbw-FEfUH-FHSfrtpprUFSBEjwrKtdme0emlQkM86t9rXsLcWW-pEMo9rZ8UF8LOG8-ZPg/exec"
   
 };
